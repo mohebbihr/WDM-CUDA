799ba0901f89c0efda7e164580d8526dc85bd528
mohebbihr <mohebbi.h@gmaill.com>
2016-10-19 01:42:16 -0400

    Update wdm.m

--- wdm.m
+++ wdm.m
@@ -1,14 +1,19 @@
+% WDM learning weighted distance metric. 
+% author: Hamidreza Mohebbi
+% Email: mohebbi.h@gmail.com
+% May, 2016
+
 function [ predlabel, acc, UProj ] = wdm( trainfea, traingnd, testfea, testgnd,options )
         		
         [ Udla1, ~, ~] = WDLAMatrix( trainfea,traingnd,options );        
-	%WDLAMatrix_org( trainfea,traingnd,options );
+	%[ Udla1, ~, ~] = WDLAMatrixCUDA( trainfea,traingnd,options ); % CUDA call
 	acc1 = evaluate_dimesion( trainfea,traingnd, trainfea,traingnd,Udla1, 1, 1);        
         
         [~, d] = max(acc1); 
         testfeaProj = testfea*Udla1(:,1:d);        
         
         [ Udla2, ~, ~] = WDLAMatrix( testfeaProj,testgnd,options );        
-        %WDLAMatrix_org( testfeaProj,testgnd,options );
+        %%[ Udla2, ~, ~] = WDLAMatrixCUDA( testfeaProj,testgnd,options ); % CUDA call
 	acc2 = evaluate_dimesion( trainfea,traingnd,trainfea,traingnd,Udla1(:,1:d)*Udla2,1, 1);        
         
         [~, d2] = max(acc2);
@@ -21,7 +26,7 @@ function [ predlabel, acc, UProj ] = wdm( trainfea, traingnd, testfea, testgnd,o
         trainProj = trainfea*UProj;
         testProj = testfeaProj*Udla2(:,1:d2);        
         
-        [ predlabel acc] = knn( trainProj,testProj,traingnd,testgnd);                      
+        [ predlabel acc] = knn( trainProj,testProj,traingnd,testgnd); % change this if you want to use another classifier                     
                         
 end
 
